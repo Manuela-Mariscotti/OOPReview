d958736baff1802b60bd7b710494223ffd87a3d1
Manuela-Mariscotti <you@example.com>
2022-07-21 23:23:01 +0200

    clase Mobile con atributos privados

--- mobile.ts
+++ mobile.ts
@@ -1,15 +1,14 @@
 export class Mobile{
-    public mobile:string;
-    public model:string;
-    public trademark:string;
-    public sdSize:number;
-    public color:string;
-    public is5G:boolean;
-    public cameraNumber:number;
-    public price:number;
-
-    constructor(mobile:string,model:string,trademark:string,sdSize:number,color:string,is5G:boolean,cameraNumber:number,price:number){
-        this.mobile = mobile;
+    private name:string;
+    private model:string;
+    private trademark:string;
+    private sdSize:number;
+    private color:string;
+    private is5G:boolean;
+    private cameraNumber:number;
+    private price:number;
+    constructor(name:string,model:string,trademark:string,sdSize:number,color:string,is5G:boolean,cameraNumber:number,price:number){
+        this.name = name;
         this.model = model;
         this.trademark = trademark;
         this.sdSize = sdSize;
@@ -18,4 +17,62 @@ export class Mobile{
         this.cameraNumber = cameraNumber;
         this.price = price;
     }
+    public getName():string{
+        return this.name;
+    }
+    public getModel():string{
+        return this.model;
+    }
+    public getTrademark():string{
+        return this.trademark;
+    }
+    public getSdSize():number{
+        return this.sdSize;
+    }
+    public getColor():string{
+        return this.color;
+    }
+    public getIs5G():boolean{
+        return this.is5G;
+    }
+    public getCameraNumber():number{
+        return this.cameraNumber;
+    }
+    public getPrice():number{
+        return this.price;
+    }
+    public setName(name:string):void{
+        this.name = name;
+    }
+    public setModel(model:string):void{
+        this.model = model;
+    }
+    public setTrademark(trademark:string):void{
+        this.trademark = trademark;
+    }
+    public setSdSize(sdSize:number):void{
+        this.sdSize = sdSize;
+    }
+    public setColor(color:string):void{
+        this.color = color;
+    }
+    public setIs5G(is5G:boolean):void{
+        this.is5G = is5G;
+    }
+    public setCameraNumber(cameraNumber:number):void{
+        this.cameraNumber = cameraNumber;
+    }
+    public setPrice(price:number):void{
+        this.price = price;
+    }
+    public toString():void{
+        console.log(`The characteristics of the mobile ${this.name} are:`);
+        console.log(`Name : ${this.name}`);
+        console.log(`Model : ${this.model}`);
+        console.log(`Trademark : ${this.trademark}`);
+        console.log(`SD Size (GB) : ${this.sdSize}`);
+        console.log(`Color : ${this.color}`);
+        console.log(`Is 5g? : ${this.is5G}`);
+        console.log(`Number of Cameras : ${this.cameraNumber}`);
+        }       
 }
--- mobileTest.ts
+++ mobileTest.ts
@@ -4,12 +4,19 @@ let Nokia3210 = new Mobile("Nokia3210","3210","Nokia",0,"azul",false,0,110);
 let iPhone3G = new Mobile("iPhone3G","3G","Apple",8,"blanco",true,1,499);
 let SamsugGalaxy10 = new Mobile("Samsug Galaxy 10","Galaxy 10","Samsung",128,"gris",true,3,437);
 
-console.log(Nokia3210);
-console.log(iPhone3G);
-console.log(SamsugGalaxy10);
+Nokia3210.setIs5G(true);
+Nokia3210.setCameraNumber(4);
 
-Nokia3210.is5G=true;
-Nokia3210.cameraNumber=4;
+console.log(Nokia3210.getIs5G());
+console.log(Nokia3210.getCameraNumber());
 
-console.log(Nokia3210);
+Nokia3210.toString();
+iPhone3G.toString();
+SamsugGalaxy10.toString();
+
+let mobiles:Mobile[];
+mobiles = [Nokia3210,iPhone3G,SamsugGalaxy10];
+mobiles[0].toString();
+mobiles[1].toString();
+mobiles[2].toString();
 
